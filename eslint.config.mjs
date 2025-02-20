76c9e175590f3221d655d0efd9f8c33117900c7b
Rinaldy <84848598+rinaldyalfariszi@users.noreply.github.com>
2025-02-20 21:46:50 +0700

    Setup Tailwind CSS & Theming

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,72 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	{
+		ignores: ['components/ui/**/*'],
+	},
+	...compat.extends(
+		'next/core-web-vitals',
+		'next/typescript',
+		'standard',
+		'plugin:tailwindcss/recommended',
+		'prettier'
+	),
+	{
+		rules: {
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin',
+						'external',
+						'internal',
+						['parent', 'sibling'],
+						'index',
+						'object',
+					],
 
-export default eslintConfig;
+					'newlines-between': 'always',
+
+					pathGroups: [
+						{
+							pattern: '@app/**',
+							group: 'external',
+							position: 'after',
+						},
+					],
+
+					pathGroupsExcludedImportTypes: ['builtin'],
+
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+			'comma-dangle': 'off',
+		},
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+
+		rules: {
+			'no-undef': 'off',
+			camelcase: ['error', { ignoreImports: true }],
+		},
+	},
+	// ...compat.plugins('import', 'promise', 'n'),
+]
+
+export default eslintConfig
