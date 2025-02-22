a31e84502b227f5f83b5b8af1bc1cb303c33d18e
Rinaldy <84848598+rinaldyalfariszi@users.noreply.github.com>
2025-02-22 18:26:43 +0700

    Assets and Metadata Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black">Welcome to the world of Next.js</h1>
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   )
 }
 
